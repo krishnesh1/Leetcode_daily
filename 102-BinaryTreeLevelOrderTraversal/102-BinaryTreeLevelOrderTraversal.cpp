85a2db383fb99b301d8083bbd2f5858f85f1b177
Krishnesh Karn <karnkrishnesh49@gmail.com>
2026-07-21 23:14:31 +0530

    Create 102-BinaryTreeLevelOrderTraversal

---
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,36 @@
+// Last updated: 21/07/2026, 23:14:27
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>result;
+        queue<TreeNode *>q;
+        q.push(root);
+        if(root==NULL) return result;
+        while(!q.empty()){
+            int n=q.size();
+            vector<int>v;
+            for(int i=0;i<n;i++){
+                TreeNode *temp=q.front();
+                q.pop();
+                v.push_back(temp->val);
+                if(temp->left)
+                q.push(temp->left);
+                if(temp->right)
+                q.push(temp->right);
+            }
+            result.push_back(v);
+        }
+        return result;
+    }
+};
