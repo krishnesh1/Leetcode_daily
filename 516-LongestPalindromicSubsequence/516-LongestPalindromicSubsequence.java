d5a1e55d9ef2bc779fc0e898a5cb87d98f3939cb
Krishnesh Karn <karnkrishnesh49@gmail.com>
2026-07-21 23:12:29 +0530

    Create 516-LongestPalindromicSubsequence

---
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.java
@@ -0,0 +1,31 @@
+// Last updated: 21/07/2026, 23:12:25
+class Solution {
+
+    public int lps(String str,int s,int e,int [][] dp){
+
+        if(s>e) return 0;
+        if(s==e) return 1;
+        if(dp[s][e]!=-1) return dp[s][e];
+        if(str.charAt(s)==str.charAt(e)){
+            dp[s][e]=2+lps(str,s+1,e-1,dp);
+        }else{
+            dp[s][e]=Math.max(lps(str,s+1,e,dp),lps(str,s,e-1,dp));
+        }
+        return dp[s][e];
+    }
+    public int longestPalindromeSubseq(String s) {
+        
+        int n=s.length();
+        int [][] dp  = new int [n][n];
+
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+
+        return lps(s,0,n-1,dp);
+
+    }
+
+    // time complexity :- o(n^2);
+    // it can be done in o(n) by using Manacher's Algorithm remember for interview
+}
