b5c68501213bb67785c2c79de68c96127b89637b
Krishnesh Karn <karnkrishnesh49@gmail.com>
2026-07-21 23:13:06 +0530

    Create 239-SlidingWindowMaximum

---
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
@@ -0,0 +1,27 @@
+// Last updated: 21/07/2026, 23:13:02
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int>ans;
+        int i=0,j=0;
+        list<int>l;
+        int n=nums.size();
+        while(j<n){
+            while(l.size()>0 && l.back()<nums[j]){
+                l.pop_back();
+            }
+            l.push_back(nums[j]);
+            if((j-i+1)<k){
+                j++;
+            }else if((j-i+1)==k){
+                ans.push_back(l.front());
+                if(nums[i]==l.front()){
+                    l.pop_front();
+                }
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
